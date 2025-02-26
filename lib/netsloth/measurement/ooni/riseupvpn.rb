68e815e765f0355fc6044cc03c985c49e20cb9a4
ziggy <ziggy@calyxinstitute.org>
2025-02-26 16:56:23 -0500

    enable ooni riseupvpn test

--- lib/netsloth/measurement/ooni/riseupvpn.rb
+++ lib/netsloth/measurement/ooni/riseupvpn.rb
@@ -7,6 +7,16 @@ module Netsloth
 
         def parse_ooni(json)
           json
+            .slice('test_start_time', 'measurement_start_time', 'probe_asn', 'test_runtime')
+            .merge({
+                     'failure' => json.dig('test_keys', 'failure'),
+                     'api_failures' => JSON.dump(json.dig('test_keys', 'api_failures')),
+                     'tcp_connect' => JSON.dump(json.dig('test_keys', 'tcp_connect'))
+                   })
+        end
+
+        def print_result
+          puts "RESULT #{self.class.display_name} api_failures: #{JSON.parse(@data['api_failures']).length}"
         end
       end
     end
